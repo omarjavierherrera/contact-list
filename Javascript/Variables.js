c463cc108d3a972c8344149463b83dfda230f176
Omar <javierpro064@gmail.com>
2023-08-18 19:00:42 -0500

    guardar rama review-1

--- Javascript/Variables.js
+++ Javascript/Variables.js
@@ -1,12 +1,11 @@
-// Definir la lista de contactos
 var listaDeContactos = [];
 
-// Función para agregar un contacto a la lista
+
 function agregarContacto(nombreApellido) {
   listaDeContactos.push(nombreApellido);
 }
 
-// Función para mostrar la lista de contactos
+
 function mostrarContactos() {
   console.log("Lista de contactos:");
   for (var i = 0; i < listaDeContactos.length; i++) {
@@ -14,14 +13,14 @@ function mostrarContactos() {
   }
 }
 
-// Función para añadir un nuevo contacto a la lista
+
 function nuevoContacto() {
   var nuevoNombreApellido = prompt("Ingrese el nombre y apellido del nuevo contacto:");
   agregarContacto(nuevoNombreApellido);
   console.log("Nuevo contacto añadido:", nuevoNombreApellido);
 }
 
-// Función para borrar un contacto de la lista
+
 function borrarContacto() {
   var contactoABorrar = prompt("Ingrese el nombre y apellido del contacto que desea borrar:");
   var indice = listaDeContactos.indexOf(contactoABorrar);
@@ -33,7 +32,7 @@ function borrarContacto() {
   }
 }
 
-// Función para imprimir en consola los contactos presentes en la lista
+
 function imprimirContactos() {
   console.log("Contactos presentes en la lista:");
   for (var i = 0; i < listaDeContactos.length; i++) {
@@ -41,19 +40,30 @@ function imprimirContactos() {
   }
 }
 
-// Agregar contactos iniciales a la lista
+
 agregarContacto("Juan Perez");
 agregarContacto("María López");
 agregarContacto("Carlos Rodriguez");
+agregarContacto("Carlos Tierra");
 
-// Mostrar la lista de contactos
+
 mostrarContactos();
 
-// Añadir un nuevo contacto
+
 nuevoContacto();
 
-// Borrar un contacto existente
+
 borrarContacto();
 
-// Imprimir los contactos presentes en la lista
+
 imprimirContactos();
+
+
+function agregarNuevoContacto() {
+  var nuevoNombreApellido = prompt("ingrese el nombre del nuevo contacto");
+  agregarContacto(nuevoNombreApellido);
+  console.log("Nuevo contacto añadido:", nuevoNombreApellido);
+}
+
+
+agregarNuevoContacto();
